fc7d7f75d2c0bc9b4a71952edc8e9423bdc775a1
weijj <JavaBeta@163.com>
2014-04-07 17:25:44 +0800

    add AOP

--- Chapter 1/aspect/AppMain.java
+++ Chapter 1/aspect/AppMain.java
@@ -11,4 +11,3 @@ public class AppMain {
 		knight.embarkOnQuest();
 	}
 }
-
--- Chapter 1/aspect/Knight.java
+++ Chapter 1/aspect/Knight.java
@@ -2,11 +2,14 @@ package aspect;
 
 public class Knight {
 	private Quest quest;
+	public Knight(){}
+	
 	public Knight(Quest quest) {
 		this.quest = quest; // dependency injection
 	}
 	
 	public void embarkOnQuest() {
 		System.out.println(this.quest.embark());
+		System.out.println("Knight saves the Damsel!");
 	}
 }
